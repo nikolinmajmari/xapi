f4256c10010239ca11799ee7fff34c2145f17518
Nikolin Majmari <nikolinmajmari30@gmail.com>
2022-05-03 14:16:02 +0200

    added in memory session adapter for sessions

--- xapi/session/mod.ts
+++ xapi/session/mod.ts
@@ -1,7 +1,7 @@
 import {RequestSession, SessionContext} from "./session.ts";
 
 export {FileAdapter} from "./file_adapter.ts";
-
+export {InMemorySessionAdapter} from "./adapter.ts";
 export {HttpCookie, getCookies, setCookie} from "./cookie.ts";
 
 export {RequestSession, SessionProvider, SessionContext} from "./session.ts";
